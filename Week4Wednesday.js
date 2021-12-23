eacb3116828d0ac37f53677b4335dd26434a9621
Sal Nast <snast@codingdojo.com>
2021-12-23 10:57:36 -0800

    Week 4 Wednesday

---
+++ Week4Wednesday.js
@@ -0,0 +1,117 @@
+// Binary Search 
+// Binary Search Tree
+
+class BSTNode {
+    constructor(value) {
+        this.val = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    //Uses a stack with iteration or relies on recursion(stack)
+    printPreOrder(curNode = this.root) {
+        if(curNode !== null) {
+            console.log(curNode.val);
+            this.printPreOrder(curNode.left);
+            this.printPreOrder(curNode.right);
+        }
+    }
+
+    printInOrder(curNode = this.root) {
+        if(curNode !== null) {
+            this.printInOrder(curNode.left);
+            console.log(curNode.val);
+            this.printInOrder(curNode.right);
+        }
+    }
+
+    printPostOrder(curNode = this.root) {
+        if(curNode !== null) {
+            this.printPostOrder(curNode.left);
+            this.printPostOrder(curNode.right);
+            console.log(curNode.val);
+        }
+    }
+
+    // Uses a queue, FIFO
+    printBreadthFirstSearch() {
+        var nodeTracker = [];
+        nodeTracker.push(this.root);
+        var currNode;
+        while(nodeTracker.length != 0) {
+            currNode = nodeTracker.shift();
+            console.log(currNode.val);
+            if (currNode.left !== null) {
+                nodeTracker.push(currNode.left);
+            }
+            if(currNode.right !== null) {
+                nodeTracker.push(currNode.right);
+            }
+        }
+    }
+    // Using recursion, determine the size of the tree
+    //myBST.size()
+    size(curNode = this.root) {
+        //base case, keep counting until the node is null
+        if(curNode === null) {
+            return 0;
+        }
+
+        return this.size(curNode.left) + this.size(curNode.right) +1;
+    }
+
+    add(val) {
+        var newNode = new BSTNode(val);
+        // If statement for the very first node to be inserted in the BST
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+
+        // Traverse the BST, to find the proper place to put the value
+        // We used a runner with SLL
+        var curr = this.root;
+
+        // if val is greater than curr.val, try to go right
+        // if curr.right does not exist, curr.right = newNode
+        // if curr.right DOES exist, curr = curr.right
+        // if val is less than curr, try to go left
+        // if curr.left does not exist, curr.left = newNode
+        // if curr.left DOES exist, curr = curr.left
+        //curr needs to keep changing, to avoid an infinite loop
+
+        while(curr) {
+            if(curr.val < val) {
+                if(curr.right === null) {
+                    curr.right = newNode;
+                    return this;
+                }
+                curr = curr.right;
+            } else {
+                if(curr.left === null) {
+                    curr.left = newNode;
+                    return this;
+                }
+                curr = curr.left;
+            }
+
+        }
+
+    }
+}
+
+var myBST = new BST();
+myBST.add(11).add(16).add(7).add(2).add(10).add(200);
+console.log("Printing using preorder traversal");
+myBST.printPreOrder();
+console.log("Printing using inorder traversal");
+myBST.printInOrder();
+console.log("Printing using postorder traversal");
+myBST.printPostOrder();
+console.log("Printing using depth first search traversal");
+myBST.printDepthFirstSearch();
--- officeHoursW4Tuesday.js
+++ officeHoursW4Tuesday.js
@@ -6,35 +6,31 @@ it is the subset of letters in each string that must be in the same order as the
 Given "abc", return an array that includes ["", "c", "b", "bc", "a", "ac", "ab", "abc"] (in any order).
 */
 
-// strSubsets("abc") - >  ["", "c", "b", "bc", "a", "ac", "ab", "abc"] 
-//strSubsets("ab") -> ["", "a", "b", "ab"]
-// strSubsets("a") -> ["", "a"]
-//strSubsets("") - > [""]
+/*
 
+Algorithm for strSubsets
+1) allCombos = [], curStr = ""
+2) allCombos.push(curStr)
+3) recursively call the function with curStr + all letters to the right of curr
+  a -> ab, ac, b -> bc, c -> 
+*/
 
 function strSubsets(str) {
     var allCombos = [];
-    //do some work...recursive work!
-    rFindAllCombos(allCombos, "", str, 0);
+    rFindAllCombos(allCombos,"",str, 0);
     return allCombos;
 }
 
 function rFindAllCombos(allCombos, curStr, originalStr, curIndex) {
-    // 1) Add the current string to the list of all combos
     allCombos.push(curStr);
-
-    // 2) Find out if we have finished processing all the letters
     if(curIndex >= originalStr.length) {
         return;
     }
 
-    // 3) To recursively call itself, with the next set of combinations
-    // "" -> "a","b","c"...."a" -> "ab","ac" ..... "ab" -> "abc".... "abc" -> STOP
-    for(var index = curIndex; index<originalStr.length; index++) {
+    for(var index = curIndex; index <originalStr.length; index++) {
         rFindAllCombos(allCombos, curStr+originalStr[index], originalStr, index+1);
-
     }
 
 }
-
 console.log(strSubsets("abc"));
+console.log(strSubsets("abcd"));
